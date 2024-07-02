eb7db6047de7cf85f68677c6cd459f73428a5211
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2024-07-02 18:37:33 -0300

    feat: add time between steps config at settings page

--- src/pages/SettingsPage/SettingsPage.tsx
+++ src/pages/SettingsPage/SettingsPage.tsx
@@ -1,6 +1,9 @@
 import {
   Fieldset,
-  Switch
+  Slider,
+  Stack,
+  Switch,
+  Text
 } from '@mantine/core'
 import {
   useContext,
@@ -16,28 +19,59 @@ import { PreloadContext } from '../../providers'
 export const SettingsPage = () => {
   const { app } = useContext(PreloadContext)
 
-  const [showUpdateNotification, setShowUpdateNotification] = useState(app.settings.showUpdateNotification)
+  const [showUpdateNotification, setShowUpdateNotification] = useState(app.settings.data.showUpdateNotification)
+  const [timeBetweenStepsInMs, setTimeBetweenStepsInMs] = useState(app.settings.data.timeBetweenStepsInMs)
+
+  const ONE_SECOND_IN_MS = 1_000
+
+  const portugueseNumberFormatter = new Intl.NumberFormat('pt-br')
 
   return (
     <Navbar>
       <Header>
-        <Fieldset legend='Atualizações' p='lg'>
-          <Switch
-            label='Exibir notificação sobre atualizações'
-            checked={showUpdateNotification}
-            onChange={
-              event => {
-                setShowUpdateNotification(event.target.checked)
+        <Stack gap='lg'>
+          <Fieldset legend='Atualizações' p='lg'>
+            <Switch
+              label='Exibir notificação sobre atualizações'
+              checked={showUpdateNotification}
+              onChange={
+                event => {
+                  setShowUpdateNotification(event.target.checked)
 
-                app.setSetting('showUpdateNotification', event.target.checked)
-                  .then(success => {
-                    if (!success)
-                      setShowUpdateNotification(!event.target.checked)
-                  })
+                  app.settings.set({ showUpdateNotification: event.target.checked })
+                    .then(success => {
+                      if (!success)
+                        setShowUpdateNotification(!event.target.checked)
+                    })
+                }
               }
-            }
-          />
-        </Fieldset>
+            />
+          </Fieldset>
+
+          <Fieldset legend='Automatizador' p='lg'>
+            <Text size='sm' mb='sm'>Tempo entre execução de cada passo</Text>
+            <Slider
+              size='sm'
+              mb='md'
+              value={timeBetweenStepsInMs}
+              defaultValue={timeBetweenStepsInMs}
+              min={0 * ONE_SECOND_IN_MS}
+              max={10 * ONE_SECOND_IN_MS}
+              step={0.1 * ONE_SECOND_IN_MS}
+              label={ms => `${portugueseNumberFormatter.format(ms / ONE_SECOND_IN_MS)} s`}
+              onChange={ms => setTimeBetweenStepsInMs(ms)}
+              onChangeEnd={ms => app.settings.set({ timeBetweenStepsInMs: ms })}
+              marks={[
+                { value: 0 * ONE_SECOND_IN_MS, label: '0 s' },
+                { value: 1 * ONE_SECOND_IN_MS, label: '1 s' },
+                { value: 2.5 * ONE_SECOND_IN_MS, label: '2,5 s' },
+                { value: 5 * ONE_SECOND_IN_MS, label: '5 s' },
+                { value: 7.5 * ONE_SECOND_IN_MS, label: '7,5 s' },
+                { value: 10 * ONE_SECOND_IN_MS, label: '10 s' }
+              ]}
+            />
+          </Fieldset>
+        </Stack>
       </Header>
     </Navbar>
   )
