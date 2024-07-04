f2badc54f66ce6f1a1a3f0635502216a4fa24ddb
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2024-07-04 20:02:21 -0300

    fix: validate edge cases at slugifyText.ts

--- src/helpers/slugifyText.ts
+++ src/helpers/slugifyText.ts
@@ -1,9 +1,34 @@
-export const slugifyText = (text: string): string => text.normalize('NFD')
-  .split('')
-  .filter(character => character.charCodeAt(0) <= 127)
-  .join('')
-  .toLowerCase()
-  .split(' ')
-  .join('-')
-  .split('_')
-  .join('-')
+export const slugifyText = (text: string): string => {
+  let slugifiedText = text.normalize('NFD')
+    .split('')
+    .filter(
+      character => {
+        const characterCode = character.charCodeAt(0)
+
+        return characterCode === ' '.charCodeAt(0) ||
+        characterCode === ','.charCodeAt(0) ||
+        characterCode === '-'.charCodeAt(0) ||
+        characterCode === '.'.charCodeAt(0) ||
+        characterCode === '_'.charCodeAt(0) ||
+        (characterCode >= '0'.charCodeAt(0) && characterCode <= '9'.charCodeAt(0)) ||
+        (characterCode >= 'A'.charCodeAt(0) && characterCode <= 'Z'.charCodeAt(0)) ||
+        (characterCode >= 'a'.charCodeAt(0) && characterCode <= 'z'.charCodeAt(0))
+      }
+    )
+    .join('')
+    .split(' ')
+    .join('-')
+    .split(',')
+    .join('.')
+    .toLowerCase()
+
+  const noExtremitiesCharacters = [' ', ',', '-', '.', '_']
+
+  while (noExtremitiesCharacters.some(character => slugifiedText.startsWith(character)))
+    slugifiedText = slugifiedText.slice(1)
+
+  while (noExtremitiesCharacters.some(character => slugifiedText.endsWith(character)))
+    slugifiedText = slugifiedText.slice(0, -1)
+
+  return slugifiedText
+}
