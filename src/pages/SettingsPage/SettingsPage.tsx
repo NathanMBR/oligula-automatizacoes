5c7fa4e97f5d3981a169eb07736da28212c90a33
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2024-07-10 00:06:00 -0300

    feat: add update button to settings page

--- src/pages/SettingsPage/SettingsPage.tsx
+++ src/pages/SettingsPage/SettingsPage.tsx
@@ -1,4 +1,5 @@
 import {
+  Button,
   Fieldset,
   Slider,
   Stack,
@@ -17,12 +18,14 @@ import {
   Navbar
 } from '../../layouts'
 import { PreloadContext } from '../../providers'
+import { UpdateModal } from '../../components'
 
 export const SettingsPage = () => {
   const { app } = useContext(PreloadContext)
 
   const [showUpdateNotification, setShowUpdateNotification] = useState(app.settings.data.showUpdateNotification)
   const [timeBetweenStepsInMs, setTimeBetweenStepsInMs] = useState(app.settings.data.timeBetweenStepsInMs)
+  const [isUpdateModalOpen, setIsUpdateModalOpen] = useState(false)
 
   const ONE_SECOND_IN_MS = 1_000
 
@@ -37,47 +40,70 @@ export const SettingsPage = () => {
       <Header>
         <Stack gap='lg'>
           <Fieldset legend='Atualizações' p='lg'>
-            <Switch
-              label='Exibir notificação sobre atualizações'
-              checked={showUpdateNotification}
-              onChange={
-                event => {
-                  setShowUpdateNotification(event.target.checked)
+            <Stack gap='xl'>
+              <Stack gap='xs'>
+                <Text size='sm'>Atualizar versão do programa</Text>
 
-                  app.settings.set({ showUpdateNotification: event.target.checked })
-                    .then(success => {
-                      if (!success)
-                        setShowUpdateNotification(!event.target.checked)
-                    })
+                <Button
+                  w='fit-content'
+                  variant='default'
+                  onClick={() => setIsUpdateModalOpen(true)}
+                  disabled={!app.update.available}
+                >
+                  {app.update.available ? 'Atualizar versão' : 'Versão atualizada'}
+                </Button>
+              </Stack>
+
+              <Switch
+                label='Exibir notificação sobre atualizações'
+                checked={showUpdateNotification}
+                onChange={
+                  event => {
+                    setShowUpdateNotification(event.target.checked)
+
+                    app.settings.set({ showUpdateNotification: event.target.checked })
+                      .then(success => {
+                        if (!success)
+                          setShowUpdateNotification(!event.target.checked)
+                      })
+                  }
                 }
-              }
-            />
+              />
+            </Stack>
           </Fieldset>
 
           <Fieldset legend='Automatizador' p='lg'>
-            <Text size='sm' mb='sm'>Tempo entre execução de cada passo</Text>
-            <Slider
-              size='sm'
-              mb='md'
-              value={timeBetweenStepsInMs}
-              defaultValue={timeBetweenStepsInMs}
-              min={0 * ONE_SECOND_IN_MS}
-              max={10 * ONE_SECOND_IN_MS}
-              step={0.1 * ONE_SECOND_IN_MS}
-              label={ms => `${portugueseNumberFormatter.format(ms / ONE_SECOND_IN_MS)} s`}
-              onChange={ms => setTimeBetweenStepsInMs(ms)}
-              onChangeEnd={ms => app.settings.set({ timeBetweenStepsInMs: ms })}
-              marks={[
-                { value: 0 * ONE_SECOND_IN_MS, label: '0 s' },
-                { value: 1 * ONE_SECOND_IN_MS, label: '1 s' },
-                { value: 2.5 * ONE_SECOND_IN_MS, label: '2,5 s' },
-                { value: 5 * ONE_SECOND_IN_MS, label: '5 s' },
-                { value: 7.5 * ONE_SECOND_IN_MS, label: '7,5 s' },
-                { value: 10 * ONE_SECOND_IN_MS, label: '10 s' }
-              ]}
-            />
+            <Stack gap='md'>
+              <Text size='sm'>Tempo entre execução de cada passo</Text>
+
+              <Slider
+                size='sm'
+                mb='md'
+                value={timeBetweenStepsInMs}
+                defaultValue={timeBetweenStepsInMs}
+                min={0 * ONE_SECOND_IN_MS}
+                max={10 * ONE_SECOND_IN_MS}
+                step={0.1 * ONE_SECOND_IN_MS}
+                label={ms => `${portugueseNumberFormatter.format(ms / ONE_SECOND_IN_MS)} s`}
+                onChange={ms => setTimeBetweenStepsInMs(ms)}
+                onChangeEnd={ms => app.settings.set({ timeBetweenStepsInMs: ms })}
+                marks={[
+                  { value: 0 * ONE_SECOND_IN_MS, label: '0 s' },
+                  { value: 1 * ONE_SECOND_IN_MS, label: '1 s' },
+                  { value: 2.5 * ONE_SECOND_IN_MS, label: '2,5 s' },
+                  { value: 5 * ONE_SECOND_IN_MS, label: '5 s' },
+                  { value: 7.5 * ONE_SECOND_IN_MS, label: '7,5 s' },
+                  { value: 10 * ONE_SECOND_IN_MS, label: '10 s' }
+                ]}
+              />
+            </Stack>
           </Fieldset>
         </Stack>
+
+        <UpdateModal
+          isOpenExternally={isUpdateModalOpen}
+          setIsOpenExternally={setIsUpdateModalOpen}
+        />
       </Header>
     </Navbar>
   )
