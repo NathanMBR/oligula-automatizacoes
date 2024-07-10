d245f56da45ab2af98f6af8cb4ede6ba76fbb76e
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2024-07-10 00:08:33 -0300

    chore: disable wip pages options at navbarItems.ts

--- src/layouts/Navbar/navbarItems.ts
+++ src/layouts/Navbar/navbarItems.ts
@@ -16,19 +16,19 @@ export const navbarItems = [
     link: '/automator/-1',
     label: 'Automatizador',
     icon: IconAutomation
-  },
-
-  {
-    link: '/inmetro-seal-generator',
-    label: 'Gerador de selo Inmetro',
-    icon: IconRubberStamp
-  },
-
-  {
-    link: '/spreadsheet-formatter',
-    label: 'Formatador de planilhas',
-    icon: IconTable
   }
+
+  // {
+  //   link: '/inmetro-seal-generator',
+  //   label: 'Gerador de selo Inmetro',
+  //   icon: IconRubberStamp
+  // },
+
+  // {
+  //   link: '/spreadsheet-formatter',
+  //   label: 'Formatador de planilhas',
+  //   icon: IconTable
+  // }
 ] as const satisfies Array<NavbarItem> // merging 'as const' with 'satisfies' to get both type enforcement and inference
 
 export type NavbarItemsLabels = typeof navbarItems[number]['label']
