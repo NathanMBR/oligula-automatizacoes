64419e97a3f0a90640252f59c818276e05787746
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2024-07-24 13:10:18 -0300

    feat: add open save folder button to automation store

--- src/pages/AutomatorPage/AutomationStore/AutomationStore.tsx
+++ src/pages/AutomatorPage/AutomationStore/AutomationStore.tsx
@@ -1,13 +1,22 @@
 import {
+  ActionIcon,
   Modal,
-  Tabs
+  Tabs,
+  Tooltip
 } from '@mantine/core'
+import {
+  IconFile,
+  IconFolder
+} from '@tabler/icons-react'
 import {
   useContext,
   useState
 } from 'react'
-import { IconFile } from '@tabler/icons-react'
 import { useParams } from 'react-router-dom'
+import {
+  path,
+  shell
+} from '@tauri-apps/api'
 
 import { FAB } from '../../../components'
 import { AutomationContext } from '../../../providers'
@@ -29,6 +38,20 @@ export const AutomationStore = () => {
     Number.isNaN(expandedStepId) ||
     expandedStepId !== -1
 
+  const openSchemasFolder = async () => {
+    try {
+      const appDataDir = await path.appDataDir()
+      const automationSchemasDir = await path.join(appDataDir, 'automations')
+
+      await shell.open(automationSchemasDir)
+    } catch (error) {
+      /* eslint-disable no-console */
+      console.error('Failed to open schemas folder:')
+      console.error(error)
+      /* eslint-enable no-console */
+    }
+  }
+
   return (
     <>
       <FAB
@@ -41,40 +64,65 @@ export const AutomationStore = () => {
         }}
       />
 
-      <Modal
+      <Modal.Root
         size='lg'
-        title='Carregar / Salvar automação'
         opened={isOpen}
         onClose={() => setIsOpen(false)}
-        overlayProps={{ blur: 2.5 }}
         centered
       >
-        <Tabs value={tabOption}>
-          <Tabs.List mb='sm' grow>
-            <Tabs.Tab
-              value='load'
-              onClick={() => setTabOption('load')}
-            >
-              Carregar
-            </Tabs.Tab>
-            <Tabs.Tab
-              value='save'
-              disabled={steps.length <= 0}
-              onClick={() => setTabOption('save')}
-            >
-              Salvar
-            </Tabs.Tab>
-          </Tabs.List>
+        <Modal.Overlay blur={2.5} />
 
-          <Tabs.Panel value='load'>
-            <LoadAutomation handleClose={() => setIsOpen(false)} />
-          </Tabs.Panel>
+        <Modal.Content>
+          <Modal.Header>
+            <Modal.Title>Carregar / Salvar automação</Modal.Title>
 
-          <Tabs.Panel value='save'>
-            <SaveAutomation handleClose={() => setIsOpen(false)} />
-          </Tabs.Panel>
-        </Tabs>
-      </Modal>
+            <Tooltip
+              label='Abrir local de salvamento'
+              position='right'
+              withArrow
+            >
+              <ActionIcon
+                ml='xs'
+                variant='subtle'
+                color='rgb(255, 255, 255)'
+                onClick={openSchemasFolder}
+              >
+                <IconFolder stroke={1} />
+              </ActionIcon>
+            </Tooltip>
+
+            <Modal.CloseButton />
+          </Modal.Header>
+
+          <Modal.Body>
+            <Tabs value={tabOption}>
+              <Tabs.List mb='sm' grow>
+                <Tabs.Tab
+                  value='load'
+                  onClick={() => setTabOption('load')}
+                >
+                  Carregar
+                </Tabs.Tab>
+                <Tabs.Tab
+                  value='save'
+                  disabled={steps.length <= 0}
+                  onClick={() => setTabOption('save')}
+                >
+                  Salvar
+                </Tabs.Tab>
+              </Tabs.List>
+
+              <Tabs.Panel value='load'>
+                <LoadAutomation handleClose={() => setIsOpen(false)} />
+              </Tabs.Panel>
+
+              <Tabs.Panel value='save'>
+                <SaveAutomation handleClose={() => setIsOpen(false)} />
+              </Tabs.Panel>
+            </Tabs>
+          </Modal.Body>
+        </Modal.Content>
+      </Modal.Root>
     </>
   )
 }
