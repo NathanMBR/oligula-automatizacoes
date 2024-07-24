8c4fc7d809b03dd03f8557e719938ba9b4f6c67e
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2024-07-24 18:55:56 -0300

    chore: add logs to rust functions

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,6 +3,8 @@
 
 use autopilot::{self, geometry::Point, mouse::Button};
 use enigo::{Direction, Enigo, Key, Keyboard, Settings};
+use log;
+use tauri_plugin_log::LogTarget;
 
 #[derive(serde::Serialize, serde::Deserialize)]
 pub struct MousePosition {
@@ -29,6 +31,8 @@ pub struct KeyboardCombination {
 
 #[tauri::command(async)]
 fn get_mouse_position() -> MousePosition {
+    log::trace!("Executing Rust function: get_mouse_position");
+
     let mouse_point = autopilot::mouse::location();
 
     MousePosition {
@@ -39,11 +43,15 @@ fn get_mouse_position() -> MousePosition {
 
 #[tauri::command(async)]
 fn check_mouse_position(position: MousePosition) -> bool {
+    log::trace!("Executing Rust function: check_mouse_position");
+
     autopilot::screen::is_point_visible(Point::new(position.x, position.y))
 }
 
 #[tauri::command(async)]
 fn move_mouse_to(position: MousePosition) -> bool {
+    log::trace!("Executing Rust function: move_mouse_to");
+
     match autopilot::mouse::move_to(Point::new(position.x, position.y)) {
         Ok(_) => true,
         Err(_) => false,
@@ -52,6 +60,8 @@ fn move_mouse_to(position: MousePosition) -> bool {
 
 #[tauri::command(async)]
 fn click(button: MouseButton) {
+    log::trace!("Executing Rust function: click");
+
     autopilot::mouse::click(
         match button {
             MouseButton::Left => Button::Left,
@@ -64,6 +74,8 @@ fn click(button: MouseButton) {
 
 #[tauri::command(async)]
 fn write(text: String) {
+    log::trace!("Executing Rust function: write");
+
     let mut enigo = Enigo::new(&Settings::default()).unwrap();
 
     enigo.text(&text).unwrap();
@@ -71,21 +83,23 @@ fn write(text: String) {
 
 #[tauri::command(async)]
 fn press_key_combination(combination: KeyboardCombination) {
+    log::trace!("Executing Rust function: press_key_combination");
+
     let mut enigo = Enigo::new(&Settings::default()).unwrap();
 
     if combination.hold_ctrl {
         enigo.key(Key::Control, Direction::Press).unwrap();
-        println!("Holding Ctrl");
+        log::trace!("Holding Ctrl");
     }
 
     if combination.hold_shift {
         enigo.key(Key::Shift, Direction::Press).unwrap();
-        println!("Holding Shift");
+        log::trace!("Holding Shift");
     }
 
     if combination.hold_alt {
         enigo.key(Key::Alt, Direction::Press).unwrap();
-        println!("Holding Alt");
+        log::trace!("Holding Alt");
     }
 
     if combination.use_unicode {
@@ -93,13 +107,13 @@ fn press_key_combination(combination: KeyboardCombination) {
             .key(Key::Unicode(combination.key_name), Direction::Click)
             .unwrap();
 
-        println!("Pressed char \"{}\" through Unicode", combination.key_name);
+        log::trace!("Pressed char \"{}\" through Unicode", combination.key_name);
     } else {
         enigo
             .key(Key::Other(combination.key_code), Direction::Click)
             .unwrap();
 
-        println!(
+        log::trace!(
             "Pressed key code \"{}\" through Other",
             combination.key_code
         );
@@ -107,22 +121,27 @@ fn press_key_combination(combination: KeyboardCombination) {
 
     if combination.hold_ctrl {
         enigo.key(Key::Control, Direction::Release).unwrap();
-        println!("Releasing Ctrl");
+        log::trace!("Releasing Ctrl");
     }
 
     if combination.hold_shift {
         enigo.key(Key::Shift, Direction::Release).unwrap();
-        println!("Releasing Shift");
+        log::trace!("Releasing Shift");
     }
 
     if combination.hold_alt {
         enigo.key(Key::Alt, Direction::Release).unwrap();
-        println!("Releasing Alt");
+        log::trace!("Releasing Alt");
     }
 }
 
 fn main() {
     tauri::Builder::default()
+        .plugin(
+            tauri_plugin_log::Builder::default()
+                .targets([LogTarget::LogDir, LogTarget::Stdout, LogTarget::Webview])
+                .build(),
+        )
         .invoke_handler(tauri::generate_handler![
             get_mouse_position,
             check_mouse_position,
